9e8a52448f5ba9161c06c0898efe0f999012c304
mhinkle98 <mrh241@pitt.edu>
2025-03-12 18:01:48 -0400

    simple changes

--- PhotonicsSuite/Hardware/TSL570.cs
+++ PhotonicsSuite/Hardware/TSL570.cs
@@ -1,7 +1,33 @@
+using NationalInstruments.Visa;
+using System;
+//using Ivi.Visa;
+
 namespace PhotonicsSuite.Hardware
 {
+    // ReSharper disable once InconsistentNaming
     public class TSL570
     {
+        private MessageBasedSession _io;
+
+        public TSL570(string resourceName = "TCPIP0::172.28.5.15::5000::SOCKET")
+        {
+            var laserManager = new ResourceManager();
+            _io = (MessageBasedSession)laserManager.Open(resourceName);
+            _io.TerminationCharacterEnabled = true;
+        }
         
+        private void Write(string cmd)
+        {
+            _io.RawIO.Write(cmd.CommandExt());
+        }
+    }
+}
+
+public static class Command
+{
+    public static string CommandExt(this string command)
+    {
+        command = string.Concat(command, "\r");
+        return command;
     }
 }
